cc2fee3b33705286f9169fd9a7d53e08650a286f
Mara Skudrite <maragr@gmail.com>
2022-03-14 17:35:02 +0200

    Day6

---
+++ src/main/scala/MyTools.scala
@@ -0,0 +1,13 @@
+object MyTools {
+  def myRound(n: Double, precision: Int=0):Double = {
+    //so the trick would be to multiply by some power of 10 then divide by that mulitplier
+    //so (n*100).round/100 would give us 2 points of precision
+    //why because built in round gives us 0 precision
+    //so if we have 100 that is 10 to the 2nd power(squared)
+    //we can use the built in Math.pow to do that
+    val multiplier = Math.pow(10, precision) //so precision 0 will give us 10 to 0 which is 1
+
+    //    n.round //only 0 precision
+    (n*multiplier).round/multiplier //we utilize the built in round
+  }
+}
