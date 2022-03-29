3fa0ab61cf7b04a9a87bc66020b77e8ad818914e
Mara Skudrite <maragr@gmail.com>
2022-03-29 20:36:57 +0300

    Home work day12 with if it likes to meet

---
+++ src/main/scala/Animal.scala
@@ -0,0 +1,35 @@
+class Animal (var animalName: String,
+              val animalType: String = "wild",
+              var animalLikes: String = "meat",
+              var animalSound: String = "Auuuuu",
+              var animalAge: Int = 0) {
+
+  def makeSound(animalSound: String): Unit = {
+    println(animalSound * 3)
+  }
+
+  def meetOrRun(animalMeet: String, likeContact: Boolean): Unit = {
+    if (likeContact) {
+      println(s"$animalName it is nice to meet You with $animalMeet")
+    } else {
+      println(s"Let's run away from $animalName and say $animalMeet")
+    }
+  }
+}
+
+
+  object Day12AnimalExercise extends App {
+    println("some changes done in Animal greetings")
+    val wolf = new Animal("wolfSecret", animalSound = "Auuuuuuuuuuuuuuu")
+    wolf.makeSound("Auu")
+    wolf.meetOrRun("nothing!",likeContact = false)
+
+    val chicken = new Animal("chichenTed", "domestic", "grains", "Cococooco")
+    chicken.makeSound("coocococooo")
+    chicken.meetOrRun("greetings, I have served some food for You!", likeContact = true)
+
+    val lion = new Animal("lionKing", "wild", animalSound="Rrrrrr")
+    lion.makeSound("rrrr")
+    lion.meetOrRun("Help!", likeContact = false)
+  }
+
