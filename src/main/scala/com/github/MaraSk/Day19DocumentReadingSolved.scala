54ee414f242f09bc53d0b3896253ae14b957c7f7
Mara Skudrite <maragr@gmail.com>
2022-04-20 13:38:04 +0300

    Day19 solution with open files

--- src/main/scala/com/github/MaraSk/Day19DocumentReadingSolved.scala
+++ src/main/scala/com/github/MaraSk/Day19DocumentReadingSolved.scala
@@ -2,6 +2,7 @@ package com.github.MaraSk
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import scala.io.Source
 
 case class Document(title:String="", author:String="", url:String="", rows:Array[String]=Array[String]()) {
   val rowCount:Int=rows.length
@@ -28,15 +29,19 @@ case class Document(title:String="", author:String="", url:String="", rows:Array
     nextLines
   }
 
-  /*
+  def getLinesFromWeb (url:String) : Array[String]={
+    val textFromWeb = Source.fromURL(url).getLines().toArray
+    textFromWeb //did not manage to close the file
+  }
+
   def save(dst:String="", folder:String="src/resources/texts"):Unit = {
-    val lines = MyTools.getTextFromWeb(url)
+    val lines = getLinesFromWeb(url)
     val newLines = appendLines(lines, url, author, title)
     val dstPath: String = if (dst == "") folder + "/" + makeFileName else folder + "/" + dst
     MyTools.saveLines(dstPath, newLines)
   }
 
-   */
+
 }
 
 object Day19DocumentReadingSolved extends App {
@@ -46,17 +51,27 @@ object Day19DocumentReadingSolved extends App {
     if (line.startsWith("https://")) line
     else "https://" + line
   }
-/*
+
+  def getLinesFromWeb (url:String) : Array[String]={
+    val html = Source.fromURL(url).getLines().toArray
+    html
+  }
   def getUrlDocuments(url:Array[String]):Array[Document] = {
     val documentArray = for (url <- url) yield {
-      val rows = MyTools.getTextFromWeb(url)
+      val rows = getLinesFromWeb(url)
       val title = GutenbergUtil.getTitle(rows)
       val author = GutenbergUtil.getAuthor(rows)
       Document(title, author, url, rows)
     }
     documentArray
   }
-
- */
+  val myDocuments = getUrlDocuments(urlContent)
+  for (doc <- myDocuments) {
+    val dstPath = doc.makeFileName
+    doc.save(dstPath)
+    println(s"You saved ${doc.title}, author: ${doc.author} from ${doc.url} to file $dstPath!")
+  }
 
 }
+
+
