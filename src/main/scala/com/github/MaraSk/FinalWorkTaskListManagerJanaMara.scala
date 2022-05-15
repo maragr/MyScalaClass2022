97de742bb1368065987aaeccfaad7235efc373fe
Mara Skudrite <maragr@gmail.com>
2022-05-15 15:15:51 +0300

    Final Work Jana&Mara start ot Task manager

---
+++ src/main/scala/com/github/MaraSk/FinalWorkTaskListManagerJanaMara.scala
@@ -0,0 +1,11 @@
+package com.github.MaraSk
+
+import scala.io.StdIn.readLine
+
+object FinalWorkTaskListManagerJanaMara extends App {
+  println("Final work assignment. TODO/TaskList manager. Authors: Jana Fedotova, Māra Skudrīte")
+  val personsName = readLine ("Welcome to TaskList manager. Please enter Your name:")
+  val selectionToDo = readLine (s"$personsName, please select what do You want to do in task manager:")
+
+
+}
