4769b4e0ef594e20698e2c319a722b6a9ca87cc3
Jana <jana.pukach@gmail.com>
2022-05-24 10:21:12 +0300

    Comments
    (cherry picked from commit e0bf785d35ce0f35df84d1426ebf489ef9d78926)

---
+++ src/main/scala/com/github/MaraSk/FinalWorkJanaMara/FinalWork.scala
@@ -0,0 +1,71 @@
+package com.github.MaraSk.FinalWorkJanaMara
+
+import scala.io.StdIn.readLine
+/**
+ * Runs taskList manager application until user choose to quit
+ *
+ * Add, print-out or delete tasks from database to user
+ * @see [[https://alvinalexander.com/scala/functional-programming-to-do-list-application/]] for more information
+ **/
+object FinalWork extends App {
+  /** Database migration
+   * created task object to call methods
+   **/
+  val db = Task("user", "task")
+  db.migrate()
+  var taskManagerActive = true
+
+  /**
+   * Asks users to enter task
+   * uses method from Task class
+   * adds task to database
+   */
+  def doAddCommand(): Unit = {
+
+    val taskInput = readLine("Enter task, please: ")
+    val firstTask = Task(userName, taskInput)
+    firstTask.addTaskToDB(userName, taskInput)
+    println("Your task have been added!")
+  }
+
+  /**
+   * Prints all users tasks with task Id
+   * Ask user which task he wants to remove
+   * uses method Task class to remove task
+   * @param userName - task manager users name
+   */
+  def deleteTask(userName:String):Unit = {
+    db.printUserTasks(userName)
+    val taskID = readLine("Enter task ID that you want to delete: ").toInt
+    db.deleteTaskFromDB(taskID)
+    println("Task was successfully removed!")
+  }
+
+  println("Final work assignment. TODO/TaskList manager. Authors: Jana Fedotova, Māra Skudrīte")
+  /** Asks user to enter name */
+  val userName = readLine("Welcome to TaskList manager. Please enter Your name: ")
+  /** Adding User to Database */
+  db.addNewUserToDatabase(userName)
+  /** /to explain what are possibilities to do with task manager */
+  db.printHelp()
+  /**
+   * main loop to choose commands till user quits taskList manager
+   * asks user to enter command
+   **/
+  while (taskManagerActive) {
+    val selectionToDo = readLine(s"$userName, please select what do You want to do in task manager: ")
+    selectionToDo.toLowerCase match {
+      case "add" => doAddCommand()
+      case "h" => db.printHelp()
+      case "v" => db.printUserTasks(userName)
+      case "rm" => deleteTask(userName)
+      case "q" =>
+        taskManagerActive = false
+        db.printQuit(userName)
+
+      case _ => println("Invalid command, please try again!")
+    }
+
+  }
+}
+
--- src/main/scala/com/github/MaraSk/FinalWorkJanaMara/Task.scala
+++ src/main/scala/com/github/MaraSk/FinalWorkJanaMara/Task.scala
@@ -2,35 +2,33 @@ package com.github.MaraSk.FinalWorkJanaMara
 
 import java.sql.{Connection, DriverManager, PreparedStatement}
 import scala.collection.mutable.ArrayBuffer
-import scala.io.StdIn.readLine
 /** A tasks assigned for user
  *
  * @constructor creates username and his task list assigning taskId
  * @param userName creates new user or find existing user by name
  * @param task adds or shows or deletes tasks according to commands
-* */
+ * */
 case class Task(userName:String, task:String, taskId:Int = 0){
 
+  val url =  s"jdbc:sqlite:src/resources/taskmanager/taskmanager.db"
+  val conn: Connection = DriverManager.getConnection(url)
+
+  /**
+   * Prints commands that user can use in taskmanager
+   */
   def printHelp():Unit = {
     println("add - add a to-do item")
     println("h - show this help text")
     println("rm - remove a task by its number")
     println("v - view the list of tasks")
     println("q - quit")
-    //println("s - show tasks (on current date or all tasks)")
   }
-
-   val url =  s"jdbc:sqlite:src/resources/taskmanager/taskmanager.db"
-
-  val conn: Connection = DriverManager.getConnection(url) //TODO handle exceptions at connection time
-  //println(s"Opened Database at ${conn.getMetaData.getURL}")
 
   /**
    * migrate for db refers to table creation other setup needed to start work in a new environment
    * @see [[https://www.sqlitetutorial.net/sqlite-create-table/]]
    **/
   def migrate():Unit = {
-
     val statement = conn.createStatement() //we create a statement object that will handle sending SQL statements to the DB
     //this query should do nothing if table already exists
     val sql =
@@ -58,9 +56,11 @@ case class Task(userName:String, task:String, taskId:Int = 0){
     statement.executeBatch()
   }
   /**
-   * creates new user in database by his name if user does not exist and assigns userid
+   * creates new user in database by his name if user does not exist
+   * assigns userid
+   * @param userName = name of the user that uses taskmanager
    **/
-  def AddNewUserToDatabase(userName:String):Unit={
+  def addNewUserToDatabase(userName:String):Unit={
     if (getUserCount(userName) == 0) {
       val insertSql = """
                         |INSERT INTO users (name,created)
@@ -75,6 +75,12 @@ case class Task(userName:String, task:String, taskId:Int = 0){
       println(s"Welcome again $userName!")
     }
   }
+
+  /**
+   * Gets user Id from database
+   * @param userName = name of the taskmanager user
+   * @return = user Id number
+   */
   def getUserId(userName:String):Int = {
     val sql =
       """
@@ -92,6 +98,12 @@ case class Task(userName:String, task:String, taskId:Int = 0){
     preparedStmt.close()
     id
   }
+
+  /**
+   * Returns user count by that name
+   * @param name user name as string
+   * @return 1 or 0
+   */
   def getUserCount(name:String):Int = {
     val sql =
       """
@@ -99,17 +111,17 @@ case class Task(userName:String, task:String, taskId:Int = 0){
         |WHERE name = ?;
         |""".stripMargin
     val preparedStmt: PreparedStatement = conn.prepareStatement(sql)
-
     preparedStmt.setString(1, name)
-
     val rs = preparedStmt.executeQuery
-
     val cnt = rs.getInt(1) //just the first column not worrying about the column name
     preparedStmt.close()
     cnt
   }
   /**
-   * adds task to identified user to database and assigns taskid and current timestamp
+   * Adds task to identified user to database
+   * Assigns task Id and current timestamp
+   * @param userName = name of the taskmanager user
+   * @param task = task that is defined by the user
    **/
   def addTaskToDB(userName:String, task:String):Unit = {
     val userid = getUserId(userName)
@@ -124,7 +136,8 @@ case class Task(userName:String, task:String, taskId:Int = 0){
     preparedStmt.close()
   }
   /**
-   * prints out all tasks to identified user from task database
+   * Saves all users tasks from database into array
+   * @param userName = name of the taskmanager user
    **/
   def showAllUsersTasks(userName:String):Array[Task]={
     val sql =
@@ -145,17 +158,23 @@ case class Task(userName:String, task:String, taskId:Int = 0){
       userBuffer += userName
     }
     userBuffer.toArray //better to return immutable values
-  } //TODO print also task number?
+  }
 
+  /**
+   * Prints out all users tasks
+   * @param userName = name of the taskmanager user
+   */
   def printUserTasks(userName:String):Unit = {
     val allTasks = showAllUsersTasks(userName)
     println(s"Here is your tasks $userName:")
     allTasks.foreach(task => println(task.printTask))
   }
+  /** Makes pretty string for task Id and task */
   def printTask:String = s"Task ID: $taskId, Task: $task"
 
   /**
-   * deletes task from identified user taskList database by task_id
+   * Deletes task from identified user taskList database by task_id
+   * @param taskID = id of a task that user wants to delete
    **/
   def deleteTaskFromDB(taskID:Int):Unit = {
     val deleteSql = """
@@ -168,15 +187,14 @@ case class Task(userName:String, task:String, taskId:Int = 0){
     preparedStmt.setInt(1, taskID)
     preparedStmt.execute
     preparedStmt.close()
-  } //FIXME does not work properly
+  }
 
   /**
-   * quits the application
+   * Quits the application
    **/
   def printQuit(userName:String): Unit = {
     println(s"Thank You $userName for using task manager! Good bye!")
   }
-  // simple notification about quiting the TaskManager
 
 
 
