e0bf785d35ce0f35df84d1426ebf489ef9d78926
Jana <jana.pukach@gmail.com>
2022-05-24 10:21:12 +0300

    Comments

--- src/main/scala/com/github/MaraSk/FinalWorkJanaMara/FinalWork.scala
+++ src/main/scala/com/github/MaraSk/FinalWorkJanaMara/FinalWork.scala
@@ -14,6 +14,12 @@ object FinalWork extends App {
   val db = Task("user", "task")
   db.migrate()
   var taskManagerActive = true
+
+  /**
+   * Asks users to enter task
+   * uses method from Task class
+   * adds task to database
+   */
   def doAddCommand(): Unit = {
 
     val taskInput = readLine("Enter task, please: ")
@@ -21,18 +27,30 @@ object FinalWork extends App {
     firstTask.addTaskToDB(userName, taskInput)
     println("Your task have been added!")
   }
+
+  /**
+   * Prints all users tasks with task Id
+   * Ask user which task he wants to remove
+   * uses method Task class to remove task
+   * @param userName - task manager users name
+   */
   def deleteTask(userName:String):Unit = {
     db.printUserTasks(userName)
     val taskID = readLine("Enter task ID that you want to delete: ").toInt
     db.deleteTaskFromDB(taskID)
     println("Task was successfully removed!")
   }
+
   println("Final work assignment. TODO/TaskList manager. Authors: Jana Fedotova, Māra Skudrīte")
+  /** Asks user to enter name */
   val userName = readLine("Welcome to TaskList manager. Please enter Your name: ")
-  db.AddNewUserToDatabase(userName) //Adding User to Database
-  db.printHelp() //to explain what are possibilities to do with task manager
+  /** Adding User to Database */
+  db.addNewUserToDatabase(userName)
+  /** /to explain what are possibilities to do with task manager */
+  db.printHelp()
   /**
    * main loop to choose commands till user quits taskList manager
+   * asks user to enter command
    **/
   while (taskManagerActive) {
     val selectionToDo = readLine(s"$userName, please select what do You want to do in task manager: ")
@@ -41,12 +59,13 @@ object FinalWork extends App {
       case "h" => db.printHelp()
       case "v" => db.printUserTasks(userName)
       case "rm" => deleteTask(userName)
-      case "q" => {
+      case "q" =>
         taskManagerActive = false
         db.printQuit(userName)
-      }
+
       case _ => println("Invalid command, please try again!")
     }
 
   }
 }
+
